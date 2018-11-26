eb63c37287dc41ec70dd5e1825f064400f4a0bb2
Mateus Soares <Usuario@10.120.15.184>
2018-11-26 14:05:14 -0200

    Começo da interface

--- deliciascaseiras/src/main/java/deliciascaseiras/modelo/Pedido.java
+++ deliciascaseiras/src/main/java/deliciascaseiras/modelo/Pedido.java
@@ -5,17 +5,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
